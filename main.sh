434139178f8db61f8f861fd030289ce253034076
Stefan Sundin <git@stefansundin.com>
2024-01-16 23:21:52 -0800

    Use `exec`.

--- main.sh
+++ main.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 chmod +x /.bottlerocket/bootstrap-containers/current/user-data
-/.bottlerocket/bootstrap-containers/current/user-data
+exec /.bottlerocket/bootstrap-containers/current/user-data
